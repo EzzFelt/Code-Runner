e85461ad9d1219ba1b34d85ef34f7d5a936640b4
Enzo Feltrin <enzobrexo@gmail.com>
2025-02-22 10:41:15 -0300

    Added comments in Layout.jsx

--- front-end/src/Components/Layout-Page/Layout.jsx
+++ front-end/src/Components/Layout-Page/Layout.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import Sidebar from './SideBar';
-import MainContent from './MainContent';
-import styles from "./styles.module.css"
+import React from 'react'; // Importing the React library
+import Sidebar from './SideBar'; // Importing the Sidebar component
+import MainContent from './MainContent'; // Importing the MainContent component
+import styles from "./styles.module.css"; // Importing CSS module for styling
 
-const Layout = () => {
+const Layout = () => { // Defining a functional component named Layout
   return (
-    <div className={styles.container}>
-      <Sidebar />
-      <MainContent />
+    <div className={styles.container}> {/* Applying CSS class from the imported styles */}
+      <Sidebar /> {/* Rendering the Sidebar component */}
+      <MainContent /> {/* Rendering the MainContent component */}
     </div>
   );
 };
 
-export default Layout;
+export default Layout; // Exporting the Layout component as the default export
